a1fed91c22c2a424bc62e1620a1943d60d356dc7
Yaswant Sai Krishna <48371142+YaswantSaiKrishna@users.noreply.github.com>
2020-10-25 10:21:12 +0530

    Update Sensor.ino

--- NodeMCUBoard/Sensor.ino
+++ NodeMCUBoard/Sensor.ino
@@ -3,12 +3,12 @@ float readSoilSensor(){
   // Channel 0 (C0 pin - binary output 0,0,0,0)
   digitalWrite(S0,LOW); digitalWrite(S1,LOW); digitalWrite(S2,LOW); digitalWrite(S3,LOW);
   float sensor0 = analogRead(SIG); /* Assign the name "sensor0" as analog output value from Channel C0 */
-  return (random(100)/100) + random(100);//sensor0;
+  return sensor0;
 }
 
 float readMQSensor(){
   // Channel 1 (C1 pin - binary output 1,0,0,0)
   digitalWrite(S0,HIGH); digitalWrite(S1,LOW); digitalWrite(S2,LOW); digitalWrite(S3,LOW);
   float sensor1 = analogRead(SIG); /* Assign the name "sensor1" as analog output value from Channel C0 */
-  return (random(100)/100) + random(100);//sensor1;
+  return sensor1;
 }
